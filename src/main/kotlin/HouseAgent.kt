7765c4348e7142e2ce7336b593067cd905772916
ooxiaoyan <810978527@qq.com>
2023-01-08 00:47:02 +0800

    [Feat] Override functions in HouseAgent

--- src/main/kotlin/HouseAgent.kt
+++ src/main/kotlin/HouseAgent.kt
@@ -1,4 +1,29 @@
 // 房屋中介
 // 在Kotlin中实现代理模式有天然优势，只需要一行代码就可以消除Java代理类中所有样板代码
 // 通过by关键字实现代理，省略了大量的样板代码，需要get✔️
-class HouseAgent(private val houseOwner: HouseOwner) : IRentOutHouse by houseOwner
+class HouseAgent(private val houseOwner: HouseOwner) : IRentOutHouse by houseOwner {
+    override fun showingHouse() {
+        houseOwner.showingHouse()
+        println("HouseAgent 领看房")
+    }
+
+    override fun getDeposit() {
+        println("HouseAgent 代收定金")
+        houseOwner.getDeposit()
+    }
+
+    override fun signContract() {
+        println("HouseAgent 提供租房协议")
+        houseOwner.signContract()
+    }
+
+    override fun getRent() {
+        houseOwner.getRent()
+        println("HouseAgent 获取佣金")
+    }
+
+    override fun provideKey() {
+        houseOwner.provideKey()
+        println("HouseAgent 代房东交接钥匙")
+    }
+}
