6716341ca4506923a9172f2983c98d0077721a24
ooxiaoyan <810978527@qq.com>
2023-01-08 16:50:07 +0800

    [Tech] Add comment to Proxy.newProxyInstance

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,9 @@ fun main(args: Array<String>) {
     val rentOutHouseIH = RentOutHouseIH(houseOwner)
     // 动态产生一个代理者
     // Proxy.newProxyInstance方法动态构造一个代理中介，需要传入被代理类的ClassLoader、共同接口集合和rentOutHouseIH实例对象
+    // 参数-ClassLoader loader，是实际被代理类的类加载器实例
+    // 参数-Class<?>[] interfaces，代理类和被代理类共同实现的接口的Class数组
+    // 参数-InvocationHandler h，代理拦截器接口，一般需要子类去实现该接口
     val dynamicProxy = Proxy.newProxyInstance(
         houseOwner.javaClass.classLoader,
         arrayOf(IRentOutHouse::class.java),
